81d654fbe4c94ccc943afe841dbecd17a8261a38
Wonseok <dannyhan8698@gmail.com>
2022-01-03 03:18:53 -0700

    Add Post Functionality

--- index.js
+++ index.js
@@ -20,14 +20,59 @@ function addListing ()
 
 function postListing ()
 {
+    let arr = [];
     let addr = $('input[name=address]').val();
-    let date = $('input[name=date]').val();
+    let pic = $('input[name=images]').val();
+    let sdate = $('input[name=sdate]').val();
+    let edate = $('input[name=edate]').val();
     let time = $('input[name=time]').val();
-    // let category = $('input[name=date]').val();
+    let check = $('input[type=checkbox]:checked');
+
+    // Post Formatting - Add Google Maps Pin
     let div = document.createElement('div');
+    div.classList.add("post-wrapper");
+    let inner = document.createElement('div');
+    inner.classList.add("post-info");
+    let img = document.createElement('img');
+    img.classList.add("post-img");
+    let addy = document.createElement('h2');
+    let span = document.createElement('span');
+    span.classList.add("post-info-wrapper");
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let p3 = document.createElement('p');
 
+    p1.appendChild(document.createTextNode("Date: " + sdate + " ~ " + edate));
+    p2.appendChild(document.createTextNode("Time: " + time));   //Change to 12 hour format with AM/PM
     
+    for (let i = 0; i < check.length; i++) {
+        if (i == check.length-1)
+        {            
+            arr += check[i].name;
+        }
+        else 
+        {            
+            arr += check[i].name + ", ";
+        }
+    }
+    p3.appendChild(document.createTextNode("Category: " + arr));
 
+    inner.append(p1);
+    inner.append(p2);
+    inner.append(p3);
+
+    img.src = pic;
+
+    span.append(img);
+    span.append(inner);
+
+    addy.appendChild(document.createTextNode(addr));
+
+    div.append(addy);
+    div.append(span);
+    $('#post').append(div);
+
+    //Use address to create Google Pin
 }
 
 function closeForm ()
